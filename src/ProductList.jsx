1b7342c1686c67b52839c6dc773e1f3f8fbb72fe
therohit15 <rohitk15102004@gmail.com>
2025-03-31 06:58:06 -0400

    Style changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -238,6 +238,7 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
         textAlign: 'center',
+        marginLeft: '50px',
     }
 
     const handleHomeClick = (e) => {
@@ -280,8 +281,8 @@ function ProductList({ onHomeClick }) {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white', marginLeft: '10px' }}>Paradise Nursery</h3>
+                                <i style={{ color: 'white', marginLeft: '10px' }}>Where Green Meets Serenity</i>
                             </div>
                         </a>
                     </div>
